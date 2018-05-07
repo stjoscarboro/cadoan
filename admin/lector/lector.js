278a8265ddd38cb723a3741c4ee0e85010cd9c0a
Andy Tran <andy.tran@ca.ibm.com>
2018-05-07 16:39:17 -0400

    .

---
+++ admin/lector/lector.js
@@ -0,0 +1,105 @@
+var app = angular.module("lectorApp", []);
+
+app.controller("LectorCtrl", ($scope, $http, $location) => {
+	
+	$scope.spreadsheetId = '1yl0oy1a9Brr2O3a9zC4HtuFnq2U9UkUZGj_A6C0YWDM';
+	$scope.sheetId = 'Sheet1';
+	$scope.apiKey = 'AIzaSyDVK5zP0TnhRam0Bsvvb59RvFZMmR3jGW8';
+	
+	/**
+	 * init
+	 */
+	$scope.init = function() {
+		$scope.lector = {}
+	}
+	
+	$scope.signin = function(info) {
+		if(info && info['Zi']) {
+			$scope.apiToken = info['Zi'].access_token;
+			$scope.get();
+		}
+	}
+	
+	/**
+	 * get
+	 */
+	$scope.get = function() {
+		let url = 'https://sheets.googleapis.com/v4/spreadsheets/' + $scope.spreadsheetId + '/values/A:C';
+		
+		$scope.clear();
+		
+		$http.get(url, {params: { key: $scope.apiKey, access_token: $scope.apiToken }})
+			.then(response => {
+				let values = response.data.values;
+				values.splice(0, 1);
+				
+				$scope.lectors = [];
+				for(let value of values) {
+					$scope.lectors.push({name: value[0], email: value[1], phone: value[2]});
+				}
+			});
+	}
+	
+	/**
+	 * create
+	 */
+	$scope.create = function() {
+		let row = $scope.lectors.length + 1,
+			range = 'A' + row + ':C' + row,
+			url = 'https://sheets.googleapis.com/v4/spreadsheets/' + $scope.spreadsheetId + '/values/A:C:append',
+			data = $scope.lector;
+		
+		let request = {
+			values: [
+				[data.name, data.email, data.phone]
+			]
+		};
+			
+		$scope.clear();
+		
+		//post
+		$http.post(url, request, {params: { key: $scope.apiKey, access_token: $scope.apiToken, valueInputOption: "USER_ENTERED" }})
+			.then(() => {
+				$scope.lectors.push(data);
+				$scope.lector = {};
+			});
+	}
+	
+	/**
+	 * delete
+	 */
+	$scope.remove = function(id) {
+		
+		let url = 'https://sheets.googleapis.com/v4/spreadsheets/' + $scope.spreadsheetId + ':batchUpdate',
+			request = {
+				"requests": [{
+					"deleteDimension": {
+						"range": {
+							"sheetId": 0,
+							"dimension": "ROWS",
+							"startIndex": id + 1,
+							"endIndex": id + 2
+						}
+					}
+				}]
+			};
+				
+		$scope.clear();
+		
+		//delete
+		$http.post(url, request, {params: { key: $scope.apiKey, access_token: $scope.apiToken }})
+			.then(() => {
+				$scope.lectors.splice(id, 1);
+			});
+	}
+	
+	/**
+	 * clear
+	 */
+	$scope.clear = function() {
+		$('.error').hide();
+		$('.rheader .rdelete').hide();
+		$("[class*='rselect']").removeClass('rselect');
+	}
+	
+});
