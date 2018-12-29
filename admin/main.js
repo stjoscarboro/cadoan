caece6dd9c4ccbac172cff962108192b3b8bf6f4
Andy Tran <andy.tran@ca.ibm.com>
2018-12-29 06:37:05 -0500

    .

---
+++ admin/main.js
@@ -0,0 +1,28 @@
+var app = angular.module("mainApp", []);
+
+app.controller("MainCtrl", ($scope, $window, $timeout, HttpService) => {
+	
+	/**
+	 * init
+	 */
+	$scope.init = function() {
+		$scope.httpService = new HttpService($scope);
+	}
+	
+	/**
+	 * signin
+	 */
+	$scope.signin = function(profile, token) {
+		$scope.profile = profile;
+		$scope.accessToken = token;
+		
+		$scope.get();
+	}
+	
+	/**
+	 * get
+	 */
+	$scope.get = function() {
+		console.log('get here');
+	}
+});
