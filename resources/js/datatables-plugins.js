e5566ff15f4801b69317cf1f68d47a5b99ec18fc
Andy Tran <andy.tran@ca.ibm.com>
2019-03-05 06:53:46 -0500

    .

--- resources/js/datatables-plugins.js
+++ resources/js/datatables-plugins.js
@@ -3,34 +3,34 @@
  */
 (function () {
 
-    function removeAccents(data) {
+    function neutralize(data) {
         return data
-            .replace(/á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/g, 'a')
-            .replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/g, 'e')
-            .replace(/í|ì|ỉ|ĩ|ị/g, 'i')
-            .replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/g, 'o')
-            .replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/g, 'u')
-            .replace(/ý|ỳ|ỷ|ỹ|ỵ/g, 'y')
-            .replace(/đ/g, 'd')
+            .replace(/[áàảãạăắằẳẵặâấầẩẫậ]/g, 'a')
+            .replace(/[ÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ]/g, 'A')
+            .replace(/[éèẻẽẹêếềểễệ]/g, 'e')
+            .replace(/[ÉÈẺẼẸÊẾỀỂỄỆ]/g, 'E')
+            .replace(/[íìỉĩị]/g, 'i')
+            .replace(/[ÍÌỈĨỊ]/g, 'I')
+            .replace(/[óòỏõọôốồổỗộơớờởỡợ]/g, 'o')
+            .replace(/[ÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ]/g, 'O')
+            .replace(/[úùủũụưứừửữự]/g, 'u')
+            .replace(/[ÚÙỦŨỤƯỨỪỬỮỰ]/g, 'U')
+            .replace(/[ýỳỷỹỵ]/g, 'y')
+            .replace(/[ÝỲỶỸỴ]/g, 'Y')
+            .replace(/[đ]/g, 'd')
+            .replace(/[Đ]/g, 'D')
             ;
     }
 
     let searchType = $.fn.DataTable.ext.type.search;
 
     searchType.string = function (data) {
-        return !data ?
-            '' :
-            typeof data === 'string' ?
-                removeAccents(data) :
-                data;
+        return !data ? '' : typeof data === 'string' ? neutralize(data) : data;
     };
 
     searchType.html = function (data) {
-        return !data ?
-            '' :
-            typeof data === 'string' ?
-                removeAccents(data.replace(/<.*?>/g, '')) :
-                data;
+        data = data.replace(/<.*?>/g, '');
+        return !data ? '' : typeof data === 'string' ? neutralize(data) : data;
     };
 
 }());
