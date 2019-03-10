2342618d3689347b83a413ddd60ec37cafd68950
Andy Tran <andy.tran@ca.ibm.com>
2019-03-09 20:20:44 -0500

    .

--- resources/js/stjoscarboro-app.js
+++ resources/js/stjoscarboro-app.js
@@ -38,7 +38,7 @@ app.factory('AppUtil', ['$document', '$interval', '$timeout', ($document, $inter
         let resize = () => {
             let contentHeight = $document.offsetParent().outerHeight();
 
-            if (contentHeight > currentHeight || Math.abs(contentHeight - currentHeight) !== 20) {
+            if (contentHeight > currentHeight || currentHeight - contentHeight !== 20) {
                 currentHeight = contentHeight + 20;
                 parent.postMessage("resize::" + currentHeight, "*");
             }
