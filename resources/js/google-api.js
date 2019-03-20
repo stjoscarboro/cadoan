37a818e9b1bff1f1c342da0c6ddef8bfc578b062
Andy Tran <andy.tran@ca.ibm.com>
2019-03-20 07:58:40 -0400

    .

--- resources/js/google-api.js
+++ resources/js/google-api.js
@@ -1,7 +1,19 @@
 let gapidiv = 'gapi-signin',
     gapiids = ['c3Rqb3NjYXJib3JvQGdtYWlsLmNvbQ==', 'aW1hbmhkdW5ndHJhbkBnbWFpbC5jb20='];
 
-function onAdminSuccess(user) {
+let renderButton = () => {
+    gapi.signin2.render(gapidiv, {
+        'scope': 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/gmail.send',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+};
+
+let onSuccess = (user) => {
     let profile = user.getBasicProfile(),
         response = user.getAuthResponse(true);
 
@@ -9,42 +21,8 @@ function onAdminSuccess(user) {
         let scope = angular.element('#' + gapidiv).scope();
         scope && scope.signin(profile, response['access_token']);
     }
-}
+};
 
-function onMemberSuccess(user) {
-    let profile = user.getBasicProfile(),
-        response = user.getAuthResponse(true);
-
-    if (profile) {
-        let scope = angular.element('#' + gapidiv).scope();
-        scope && scope.signin(profile, response['access_token']);
-    }
-}
-
-function onFailure(error) {
+let onFailure = (error) => {
     console.log(error);
-}
-
-function renderAdminButton() {
-    gapi.signin2.render(gapidiv, {
-        'scope': 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/gmail.send',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onAdminSuccess,
-        'onfailure': onFailure
-    });
-}
-
-function renderMemberButton() {
-    gapi.signin2.render(gapidiv, {
-        // 'scope': 'https://www.googleapis.com/auth/spreadsheets',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onMemberSuccess,
-        'onfailure': onFailure
-    });
-}
+};
